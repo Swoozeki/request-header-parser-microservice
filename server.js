5352a34e4e8c77f0c2f5c018c975cf240cf2f44b
Swoozeki <ka.sujit@gmail.com>
2016-07-11 03:01:29 +0000

    finished program

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var express=require('express')
 var app=express()
 
-app.get('/',function(req,res){
-    res.end("Hello World!")
+app.get('/whoami',function(req,res){
+    console.log(req.headers)
+    function getLanguage(){
+        var language=req.headers['accept-language']
+        return language.match(/..-../).join("")
+    }
+    function getSoftware(){
+        var software=req.headers['user-agent']
+        return software.slice(software.search(/\(/)+1,software.search(/\)/))
+    }
+    var sysinfo={
+        ipaddress: req.headers['x-forwarded-for'],
+        language: getLanguage(),
+        software: getSoftware()
+    }
+    res.end(JSON.stringify(sysinfo))
 })
 app.listen(8080,()=>console.log("listening to port 8080"))
